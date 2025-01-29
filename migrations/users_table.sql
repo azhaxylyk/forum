f98a879060a00ea64a80b7e888debcd7fd0b847a
azhaxylyk <zhaxylykova.a7@gmail.com>
2025-01-29 19:52:37 +0500

    update db

--- migrations/users_table.sql
+++ migrations/users_table.sql
@@ -8,3 +8,7 @@ CREATE TABLE IF NOT EXISTS users (
     role VARCHAR(20) NOT NULL DEFAULT 'user', -- 'user', 'moderator', 'admin'
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+INSERT INTO users (id, email, username, password, provider, role) 
+VALUES ('00000000-0000-0000-0000-000000000000', 'admin@gmail.com', 'admin', '$2a$10$KIX/Y6wVKTKuB1XJh8RUyO7EN0H3aY5h6F9q/NUz5JavjH1KT0D6C', 'local', 'admin')
+ON CONFLICT (email) DO NOTHING;
